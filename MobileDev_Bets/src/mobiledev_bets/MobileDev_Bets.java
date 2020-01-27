1221febbc2cbb3299037bf25582e339ddc935eba
SuccessNyameni <36991635+SuccessNyameni@users.noreply.github.com>
2020-01-27 09:03:10 +0200

    Update MobileDev_Bets.java

--- MobileDev_Bets/src/mobiledev_bets/MobileDev_Bets.java
+++ MobileDev_Bets/src/mobiledev_bets/MobileDev_Bets.java
@@ -16,6 +16,7 @@ public class MobileDev_Bets
         int winningnumber = (1 +(int)(Math.random()*10));
         JOptionPane.showMessageDialog(null, "hELOO pLAYER");
         JOptionPane.showMessageDialog(null,"The Wininmg Number Is " + winningnumber);
+       JOptionPane.showMessageDialog(null,"Success Nyameni is the winner ");
         //sTART HERE
         
     }
